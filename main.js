2c72782dd2b5cf2611c1b5b533bfeb2ced26e518
Rafael Silveira <rsdsilveira@gmail.com>
2015-11-06 19:17:24 -0200

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,23 @@ server.listen(8000);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
+
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('sqlite3.db');
+var check;
+db.serialize(function() {
+
+  db.run("CREATE TABLE if not exists user_info (info TEXT)");
+  var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
