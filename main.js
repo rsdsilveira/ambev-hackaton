e56b3f3847fe774631626aa44e36f226dddb45f5
Rafael Silveira <rsdsilveira@gmail.com>
2015-11-06 19:34:57 -0200

    Update main.js

--- main.js
+++ main.js
@@ -14,21 +14,20 @@ server.listen(8000);
 console.log("Server running at http://127.0.0.1:8000/");
 
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('sqlite3.db');
-var check;
-db.serialize(function() {
 
-  db.run("CREATE TABLE if not exists user_info (info TEXT)");
-  var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
+var db = new SQL.Database();
+// Run a query without reading the results
+db.run("CREATE TABLE test (col1, col2);");
+// Insert two rows: (1,111) and (2,222)
+db.run("INSERT INTO test VALUES (?,?), (?,?)", [1,111,2,222]);
 
-  db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
-});
+// Prepare a statement
+var stmt = db.prepare("SELECT * FROM test WHERE col1 BETWEEN $start AND $end");
+stmt.getAsObject({$start:1, $end:1}); // {col1:1, col2:111}
 
-db.close();
+// Bind new values
+stmt.bind({$start:1, $end:2});
+while(stmt.step()) { //
+    var row = stmt.getAsObject();
+    // [...] do something with the row of result
+}
